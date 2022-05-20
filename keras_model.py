72ceb3ce29e71e271b8afb255cf7d5d95def394f
Gal Tandeitnik <galtan121@gmail.com>
2022-05-20 23:17:00 +0300

    create class for keras models

---
+++ keras_model.py
@@ -0,0 +1,16 @@
+from tensorflow import keras
+
+class keras_model:
+    def __init__(self):
+        # Load preprocessing layer
+        self.preprocessing_layer = keras.models.load_model('.\Models\preprocessing_layer')
+
+        # Load encoder model
+        self.encoder_model = keras.models.load_model('.\Models\encoder_model')
+
+    
+    def preprocess_and_encode(self, item_features_dict):
+        p_item = self.preprocessing_layer(item_features_dict)
+        encoded_item = self.encoder_model(p_item)
+
+        return encoded_item
