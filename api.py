bbdf9220e2ff5ea4d2fcd604a0abe4b525dc6e4a
Gal Tandeitnik <galtan121@gmail.com>
2022-05-27 20:32:29 +0300

    fix quotes

--- api.py
+++ api.py
@@ -14,7 +14,7 @@ def welcome():
     return "pong!"
 
 
-@app.route("/analyze", methods=["POST"])
+@app.route('/analyze', methods=['POST'])
 def analyze():
     req = request.get_json()
 
@@ -23,28 +23,28 @@ def analyze():
     return jsonify(analyzed_user_data), 200
 
 
-@app.route("/resources/titles", methods=['GET'])
+@app.route('/resources/titles', methods=['GET'])
 def titles():
     titles = data_frame_manager.get_unique_titles()
 
     return jsonify(titles=titles)
 
 
-@app.route("/resources/degree/types", methods=['GET'])
+@app.route('/resources/degree/types', methods=['GET'])
 def degree_types():
     degree_types = data_frame_manager.get_unique_degree_types()
 
     return jsonify(types=degree_types)
 
 
-@app.route("/resources/degree/fields", methods=['GET'])
+@app.route('/resources/degree/fields', methods=['GET'])
 def degree_fields():
     degree_fields = data_frame_manager.get_unique_degree_fields()
 
     return jsonify(fields=degree_fields)
 
 
-@app.route("/resources/degree/institutions", methods=['GET'])
+@app.route('/resources/degree/institutions', methods=['GET'])
 def degree_institutions():
     degree_institutions = data_frame_manager.get_unique_degree_institutions()
 
