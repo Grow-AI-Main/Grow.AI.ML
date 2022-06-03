9138301e2315e1d94aa71532be520e04c2981783
Gal Tandeitnik <galtan121@gmail.com>
2022-06-03 13:35:51 +0300

    fix for linux path

--- keras_model.py
+++ keras_model.py
@@ -3,10 +3,10 @@ from tensorflow import keras
 class KerasModel:
     def __init__(self):
         # Load preprocessing layer
-        self.preprocessing_layer = keras.models.load_model('.\Models\preprocessing_layer')
+        self.preprocessing_layer = keras.models.load_model('./Models/preprocessing_layer')
 
         # Load encoder model
-        self.encoder_model = keras.models.load_model('.\Models\encoder_model')
+        self.encoder_model = keras.models.load_model('./Models/encoder_model')
 
     
     def preprocess_and_encode(self, item_features_dict):
